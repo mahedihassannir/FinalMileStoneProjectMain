72089ab62e9cbef5817af975a70b43de125c34b3
mahedi6666655555 <mahedinir34678@gmail.com>
2023-05-24 22:41:17 +0600

    captcha done

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,14 +1,32 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Footer from "../pages/Shared/Footer/Footer";
 import NavBar from "../pages/Shared/NavBar/NavBar";
 
 
+
+
+
+
+
 const Main = () => {
+    const location = useLocation()
+
+
+    const NoheaderFooter = location.pathname.includes('login')
+
+    console.log(NoheaderFooter);
+
     return (
         <div>
-            <NavBar></NavBar>
+            {NoheaderFooter ||
+                <NavBar></NavBar>
+            }
             <Outlet></Outlet>
-            <Footer></Footer>
+            {
+                NoheaderFooter ||
+                <Footer></Footer>
+
+            }
         </div>
     );
 };
--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,9 +1,20 @@
 import { Helmet } from "react-helmet-async";
-import Banner from "../Banner/Banner";
+// import Banner from "../Banner/Banner";
 import Category from "../Category/Category";
 import Featured from "../Featured/Featured";
 import PopularMenu from "../PopularMenu/PopularMenu";
 import Testimonials from "../Testimonials/Testimonials";
+
+import { Carousel } from 'react-responsive-carousel';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+
+import img1 from '../../../assets/home/01.jpg';
+import img2 from '../../../assets/home/02.jpg';
+import img3 from '../../../assets/home/03.png';
+import img4 from '../../../assets/home/04.jpg';
+import img5 from '../../../assets/home/05.png';
+import img6 from '../../../assets/home/06.png';
+
 
 const Home = () => {
     return (
@@ -11,7 +22,28 @@ const Home = () => {
             <Helmet>
                 <title>Bistro Boss | Home</title>
             </Helmet>
-            <Banner></Banner>
+            <div>
+                <Carousel className="text-center">
+                    <div>
+                        <img src={img1} />
+                    </div>
+                    <div>
+                        <img src={img2} />
+                    </div>
+                    <div>
+                        <img src={img3} />
+                    </div>
+                    <div>
+                        <img src={img4} />
+                    </div>
+                    <div>
+                        <img src={img5} />
+                    </div>
+                    <div>
+                        <img src={img6} />
+                    </div>
+                </Carousel>
+            </div>
             <Category></Category>
             <PopularMenu></PopularMenu>
             <Featured></Featured>
