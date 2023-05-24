55fdee257634a3bbac3399f3561f71f9537e52aa
mahedi6666655555 <mahedinir34678@gmail.com>
2023-05-24 23:10:15 +0600

    firebase setup and contex maek done

--- src/components/AuthProvider/ContexSuplier.jsx
+++ src/components/AuthProvider/ContexSuplier.jsx
@@ -1,7 +1,71 @@
 
-const ContexSuplier = ({children}) => {
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithPhoneNumber, signOut } from 'firebase/auth'
 
-    return 
+const auth = getAuth(app)
+
+import { createContext, useEffect, useState } from 'react';
+import app from './firebase.config';
+
+export const contexM = createContext(null)
+
+const ContexSuplier = ({ children }) => {
+
+    // here is user and login systems
+
+    const [user, SetUser] = useState(null)
+
+    const [Loader, SetLoader] = useState(true)
+
+    // here is user and login systems ends
+
+
+
+    const LoginUser = (email, password) => {
+        SetLoader(true)
+        return signInWithPhoneNumber(auth, email, password)
+    }
+
+    const createUser = (email, password) => {
+        SetLoader(true)
+
+        return createUserWithEmailAndPassword(auth, email, password)
+    }
+
+    const LogoutUser = () => {
+        return signOut(auth)
+    }
+
+
+
+    // here is teh user observer 
+
+
+    useEffect(() => {
+        const off = onAuthStateChanged(auth, watch => {
+            SetUser(watch)
+            console.log('user is ob obserbing', watch);
+            SetLoader(false)
+        })
+        return () => {
+            off
+        }
+    }, [])
+
+
+    const userInfos = {
+        LoginUser,
+        createUser,
+        LogoutUser,
+        Loader,
+        user
+
+
+    }
+
+
+    return <contexM.Provider value={userInfos}>
+        {children}
+    </contexM.Provider>
 
 };
 
--- src/main.jsx
+++ src/main.jsx
@@ -6,13 +6,17 @@ import {
 } from "react-router-dom";
 import { router } from './Routes/Routes';
 import { HelmetProvider } from 'react-helmet-async';
+import ContexSuplier from './components/AuthProvider/ContexSuplier';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HelmetProvider>
-      <div className='max-w-screen-xl mx-auto'>
-        <RouterProvider router={router} />
-      </div>
-    </HelmetProvider>
+    <ContexSuplier>
+
+      <HelmetProvider>
+        <div className='max-w-screen-xl mx-auto'>
+          <RouterProvider router={router} />
+        </div>
+      </HelmetProvider>
+    </ContexSuplier>
   </React.StrictMode>,
 )
